012b975ac4fe9b2992cb5408fc8c916bffb3cc33
Bruno Barros <contato4code@gmail.com>
2022-09-30 00:43:41 -0300

    init project

---
+++ src/main/java/com/moneyawapi/MoneyawApiApplication.java
@@ -0,0 +1,13 @@
+package com.moneyawapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyawApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyawApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moneyawapi/MoneyawApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneyawapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyawApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
