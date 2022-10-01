17a86f3015e8b7552061709e9f97cafbd7010467
Bruno Barros <contato4code@gmail.com>
2022-09-30 22:59:32 -0300

    CRUD Categorias

--- src/main/java/com/moneyawapi/resource/CategoriaResource.java
+++ src/main/java/com/moneyawapi/resource/CategoriaResource.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
@@ -23,9 +27,20 @@ public class CategoriaResource {
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void criar(@RequestBody Categoria categoria) {
+//    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria) {
         Categoria categoriaSalve = categoriaRepository.save(categoria);
+        // create URI path to the created component in Headers Location
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(categoriaSalve.getCodigo()).toUri();
+        return ResponseEntity.created(uri).body(categoriaSalve);
+
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<?> buscarPeloCodigo(@PathVariable long codigo) {
+        Optional<Categoria> categoria = categoriaRepository.findById(codigo);
+        return categoria.isPresent() ? ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
     }
 
 }
