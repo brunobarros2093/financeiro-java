c586a47ef874adf7f8ccaaa36d663bcecc2c75c4
Bruno Barros <contato4code@gmail.com>
2022-10-08 20:47:46 -0300

    POST Lancamento

--- src/main/java/com/moneyawapi/resource/LancamentoResource.java
+++ src/main/java/com/moneyawapi/resource/LancamentoResource.java
@@ -1,12 +1,16 @@
 package com.moneyawapi.resource;
 
+import com.moneyawapi.event.ResourceCriadoEvent;
 import com.moneyawapi.model.Lancamento;
 import com.moneyawapi.repository.LancamentosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,6 +21,8 @@ public class LancamentoResource {
     @Autowired
     private LancamentosRepository lancamentosRepository;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
@@ -28,6 +34,14 @@ public class LancamentoResource {
     public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
         Optional<Lancamento> lancamento = lancamentosRepository.findById(codigo);
         return lancamento.isPresent() ? ResponseEntity.ok(lancamento.get()) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Lancamento> cadastrarLancamento(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+        Lancamento lanc = lancamentosRepository.save(lancamento);
+        publisher.publishEvent(new ResourceCriadoEvent(this, response, lanc.getCodigo()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(lanc);
+
     }
 
 }
