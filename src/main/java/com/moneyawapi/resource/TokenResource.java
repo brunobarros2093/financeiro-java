4f806d5eb45d9b0df68db37a2f193a5387dc7ccb
Bruno Barros <contato4code@gmail.com>
2022-10-19 14:32:08 -0300

    Revoke token

--- src/main/java/com/moneyawapi/resource/TokenResource.java
+++ src/main/java/com/moneyawapi/resource/TokenResource.java
@@ -1,10 +1,27 @@
 package com.moneyawapi.resource;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/tokens")
 public class TokenResource {
-    
+
+    @DeleteMapping("/revoke")
+    public void revoke(HttpServletRequest req, HttpServletResponse res) {
+        Cookie cookie = new Cookie("refreshToken", null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false); // TODO em prod = true
+        cookie.setPath(req.getContextPath() + "/oauth/token");
+        cookie.setMaxAge(0);
+        res.addCookie(cookie);
+        res.setStatus(HttpStatus.NO_CONTENT.value());
+    }
+
 }
